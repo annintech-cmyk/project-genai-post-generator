6ff385dacb543e8efb3f52dfb01be873c8359547
annintech-cmyk <ann.intech@gmail.com>
2026-04-29 14:33:17 +0200

    save french language -commit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ language_options = ["English", "French"]
 
 # Main app layout
 def main():
-    st.subheader("LinkedIn Post Generator: Codebasics")
+    st.subheader("LinkedIn Post Generator: Hi there")
 
     # Create three columns for the dropdowns
     col1, col2, col3 = st.columns(3)
