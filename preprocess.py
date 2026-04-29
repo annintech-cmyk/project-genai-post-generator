36a4d9476b2a9c4a8ce92ae5de50511ac7342e39
annintech-cmyk <ann.intech@gmail.com>
2026-04-29 14:28:55 +0200

    save french language -commit

--- preprocess.py
+++ preprocess.py
@@ -30,7 +30,7 @@ def extract_metadata(post):
     1. Return a valid JSON. No preamble. 
     2. JSON object should have exactly three keys: line_count, language and tags. 
     3. tags is an array of text tags. Extract maximum two tags.
-    4. Language should be English or Hinglish (Hinglish means hindi + english)
+    4. Language should be English or French
 
     Here is the actual post on which you need to perform this task:  
     {post}
